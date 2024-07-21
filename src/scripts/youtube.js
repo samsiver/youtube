b65cb3c2814baa3d2d28bd630bfbf743a15d0415
axesman <138464938+samsiver@users.noreply.github.com>
2024-07-21 12:24:15 -1000

    chenge

---
+++ src/scripts/youtube.js
@@ -0,0 +1,26 @@
+function createElements(eventEl){
+       let subsMe = document.createElement("div");
+       subsMe.className = "sub";
+
+              let imged = document.createElement("img");
+              imged.src = "../../img/photos/" + eventEl.pic[0];
+
+              let nameSubed = document.createElement("span");
+              nameSubed.className = "nameSub";
+              nameSubed.innerHTML = eventEl.name;
+
+       subsMe.appendChild(imged);
+       subsMe.appendChild(nameSubed);
+
+       Subscribers.appendChild(subsMe);
+}
+let numbers = 0;
+function fillGrid(evented){
+       Subscribers.innerHTML = "";
+       for(i=0; i<evented.length; i++){
+              numbers = numbers +1;
+              numSuber.innerHTML = numbers;
+              createElements(evented[i]);
+       }
+}
+fillGrid(subs);
