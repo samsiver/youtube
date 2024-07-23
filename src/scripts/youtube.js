a9b99ccdbf2f4ec7ea41023a1f38d71b3972a895
axesman <138464938+samsiver@users.noreply.github.com>
2024-07-23 00:23:41 -1000

    chenge

--- src/scripts/youtube.js
+++ src/scripts/youtube.js
@@ -63,8 +63,10 @@
 
 
 function createElements(eventEl) {
-              let subsMe = document.createElement("div");
+       let subsMe = document.createElement("div");
        subsMe.className = "sub";
+
+       subsMe.addEventListener("click",()=>{showProf(eventEl.id)});
 
               let imged = document.createElement("img");
               imged.src = "../../img/photos/" + eventEl.pic[0];
@@ -125,7 +127,7 @@ function fillGrid(evented) {
 fillGrid(subs);
 
 var videosds = document.querySelectorAll(".vid");
-
+var videosds2 = document.querySelectorAll(".vid2");
 function playedPause(event){
        event.onmousemove = function(){
               event.play();
@@ -138,3 +140,79 @@ function playedPause(event){
 videosds.forEach(el =>
        playedPause(el)
 );
+
+function createElements2(event , ell2){
+       let videoPlayListed2 = document.createElement("div");
+              videoPlayListed2.className = "videoPlayList2";
+
+              let vided2 = document.createElement("video");
+              vided2.className = "vid2";
+              vided2.src = "../../img/videos/" + ell2;
+              videoPlayListed2.appendChild(vided2);
+
+              let titlesIn2 = document.createElement("div");
+              titlesIn2.className = "titles2";
+
+              let imged3 = document.createElement("img");
+              imged3.src = "../../img/photos/" + event.pic[0];
+
+              let nameIs2 = document.createElement("span");
+              nameIs2.className = "nameTitle2";
+              nameIs2.innerHTML = event.name;
+
+              let userNameIs2 = document.createElement("span");
+              userNameIs2.className = "userName2";
+              userNameIs2.innerHTML = event.name;
+
+       titlesIn2.appendChild(imged3);
+       titlesIn2.appendChild(nameIs2);
+       titlesIn2.appendChild(userNameIs2);
+
+       videoPlayListed2.appendChild(titlesIn2);
+
+       moveIs.appendChild(videoPlayListed2);
+}
+
+var numPosts = 0;
+function showProf(pid){
+       let subProfed = subs.find(el => el.id == pid);
+
+       profiledBio.className = "";
+
+       bnrProfed.src = "../../img/banner/" + subProfed.banner;
+       photoProfed.src = "../../img/photos/" + subProfed.pic;
+       nameed.innerHTML = subProfed.name;
+       // userNamed.innerHTML = subProfed.username;
+
+       sbscrib.innerHTML = subProfed.subs + "M";
+
+       moveIs.innerHTML = "";
+       numPosts = "";
+       for(ell of subProfed.videosIn){
+              numPosts = numPosts +1;
+                     videoerIn.innerHTML = numPosts;
+              createElements2(subProfed , ell);
+       }
+}
+
+videosds2.forEach(el =>
+       playedPause(el)
+);
+
+var clickShowMore = 0;
+tagNumSub.onclick = function(){
+       if(clickShowMore == 0){
+              clickShowMore = 1;
+              Subscribers.style.overflowY = "auto";
+              Subscribers.style.height = "250px";
+              tagNumSub.innerHTML = "Show less";
+       }
+       else {
+              clickShowMore = 0;
+              Subscribers.style.overflowY = "";
+              Subscribers.style.height = "";
+              tagNumSub.innerHTML = "Show more";
+       }
+}
+
+// vid3.play()
